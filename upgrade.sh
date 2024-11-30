19d3d7d7e5ac83bdc29aa2d08fd8486cedd7ada3
REN9999 <sejatipria990@gmail.com>
2024-11-30 13:26:02 +0700

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+# Tampilkan pesan
+echo "Memulai upgrade..."
 pkg upgrade
+
+# Tunggu pengguna sebelum kembali ke menu utama
+read -p "Tekan Enter untuk kembali ke menu utama."
+./menu.sh

2edb66307243fdb5f67830109de0ac79401791df
REN9999 <sejatipria990@gmail.com>
2024-11-30 13:08:31 +0700

    Update update123.sh

--- update123.sh
+++ update123.sh
@@ -1,1 +1,13 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
+
+read -p "pencet enter untuk melanjutkan script"
 pkg update
+read -p "pencet enter untuk kembali ke menu awal"
+./file1

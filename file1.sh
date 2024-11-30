99eca15d6ee4ed21b6d87d8f56ea745990c67048
REN9999 <sejatipria990@gmail.com>
2024-11-30 13:30:08 +0700

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -13,10 +13,13 @@ read -p "Pilih menu (1/2/3): " pilihan
 
 # Kondisi untuk setiap pilihan
 if [ "$pilihan" == "1" ]; then
+chmod +x update123.sh
     ./update123.sh
 elif [ "$pilihan" == "2" ]; then
+chmod +x upgrade.sh
     ./upgrade.sh
 elif [ "$pilihan" == "3" ]; then
+chmod +x exit.sh
     ./exit.sh
 else
     echo "Pilihan tidak valid! Silakan coba lagi."

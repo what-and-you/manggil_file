f68e124208153d768a246c58e637d3099c53f96b
REN9999 <sejatipria990@gmail.com>
2024-11-30 13:25:47 +0700

    Update update123.sh

--- update123.sh
+++ update123.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-YELLOW='\033[0;33m'
-WHITE='\033[1;37m'
-RESET='\033[0m'
+# Tampilkan pesan
+echo "Memulai upgrade..."
+pkg upgrade
 
-echo -e "${BLUE}pencet enter untuk melanjutkan script${RESET}"
-read -p ""
-echo "hallo word"
-echo -e "${CYAN}pencet enter untuk kembali ke menu awal${RESET}"
-read -p ""
+# Tunggu pengguna sebelum kembali ke menu utama
+read -p "Tekan Enter untuk kembali ke menu utama."
+./menu.sh

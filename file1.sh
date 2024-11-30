13fd0717d76b6d0333819732ce8cd9a2c7f2ab2d
REN9999 <sejatipria990@gmail.com>
2024-11-30 13:06:39 +0700

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -1,9 +1,13 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
+
 clear
-echo
-echo
-echo
-echo
-echo
 echo
 echo "
 1.update
